d8eecc206ceceef5e25d943d2d4b8a8981309b08
Panibrat <a.panibratenko@gmail.com>
2017-05-25 19:42:00 +0300

    minor logic changes

--- improvedFighter.js
+++
@@ -1,9 +0,0 @@
-import Fighter from './mprovedFighter';
-
-//место для комментариев к классу
-export default class ImprovedFighter extends Fighter{
-    doubleHit(enemy, point = 0){
-        point *= 2;
-        super.hit(enemy, point);
-    }
-};
